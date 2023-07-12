9b4b6c64a0337c187d28eddb724ba235b9793f09
Jeetin kumar anand <kumaranand8126@gmail.com>
2023-07-12 18:38:42 +0530

    Choosing Image From Gallery in Android

--- app/src/main/java/com/example/cameraappprototype/MainActivity.java
+++ app/src/main/java/com/example/cameraappprototype/MainActivity.java
@@ -1,15 +1,68 @@
 package com.example.cameraappprototype;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.security.PrivateKey;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
+    private Button galleryBtn, cameraBtn;
+
+
+    ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Uri image_uri = result.getData().getData();
+                        imageView.setImageURI(image_uri);
+                    }
+
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        galleryBtn = findViewById(R.id.button);
+        cameraBtn = findViewById(R.id.button2);
+
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryActivityResultLauncher.launch(galleryIntent);
+            }
+        });
+
+
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
